f3e4c5a1c9372a3658344a5208e643adde8caedc
sophiaborsatto <sophia-borsatto@hotmail.com>
2025-06-05 20:29:44 -0300

    mercadinho0506

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 //catalogo
-
+const div =  document.getElementById('catalogo');
 //assincrono
 async function fetchProdutos(){
     try {
-                        //aguarde a resposta
+                        //aguarde a resposta / espere
         const resposta = await fetch('https://fakestoreapi.com/products');
-        const listaProdutos = resposta.json();
+        const listaProdutos = await resposta.json();
         console.log(listaProdutos);
+        imprimirVetor(listaProdutos);
     } catch (error) {
         console.error("Erro na api: ", error);
-        divCatalogo.innerHTML = "<h1>Site indisponivel, tente novamente.</h1>";
-
+        //para fazer um aviso de erro:
+        let h1 = document.createElement('h1');
+        h1.textContent = 'Indisponivel. Tente novamente mais tarde.';
+        div.appendChild(h1);
+        //ou:
+        //divCatalogo.innerHTML = "<h1>Site indisponivel, tente novamente.</h1>";
     }
+}
+function imprimirVetor(vetor) {
+    div.innerHTML = "";
+
+    vetor.forEach(element => {
+        const divProduto = document.createElement('div');
+        divProduto.className = "itemProduto";
+
+        divProduto.innerHTML = `
+            <img src="${element.image}" alt="produto">
+            <h2>${element.title}</h2>
+            <button onclick="comprar()">Comprar</button>
+        `;      //para chamar um elemento
+        div.appendChild(divProduto);
+    });
 }
 fetchProdutos();
